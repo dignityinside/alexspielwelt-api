eb2964b63f7159f20f144dfba0d22095d34d366c
Alexander Schilling <dignityinside@protonmail.com>
2025-01-02 22:48:27 +0100

    Add global protected routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from '@/auth/auth.service';
-import { AuthGuard } from '@/auth/auth.guard';
+import { Public } from '@/auth/public.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -8,11 +8,11 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
+  @Public()
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.username, signInDto.password);
   }
 
-  @UseGuards(AuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { AppConfigService } from '@/app/config/config.service';
 import { AuthController } from '@/auth/auth.controller';
 import { AuthService } from '@/auth/auth.service';
 import { UsersModule } from '@/users/users.module';
+import { AuthGuard } from '@/auth/auth.guard';
 
 @Module({
   imports: [
@@ -21,7 +23,14 @@ import { UsersModule } from '@/users/users.module';
       }),
     }),
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    // apply auth guard globally for all routes
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   controllers: [AuthController],
   exports: [AuthService],
 })
